fc073940dad7316004e9e1265ce62284877110e8
Stephan Bluitt <46945764+stephanbluitt@users.noreply.github.com>
2026-03-15 17:18:04 -0400

    Add temperature conversion functions for Fahrenheit and Kelvin

--- conversions.py
+++ conversions.py
@@ -1,13 +1,34 @@
-
 def convertCelsiusToKelvin(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"""
-    kelvins = 0
-    
+    kelvins = celsius + 273.15
     return kelvins
 
 
 def convertCelsiusToFahrenheit(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
-    fahrenheit = 0
-    
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+
+def convertFahrenheitToCelsius(fahrenheit):
+    """Converts Fahrenheit to Celsius"""
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+
+def convertFahrenheitToKelvin(fahrenheit):
+    """Converts Fahrenheit to Kelvin"""
+    kelvin = (fahrenheit - 32) * 5/9 + 273.15
+    return kelvin
+
+
+def convertKelvinToCelsius(kelvin):
+    """Converts Kelvin to Celsius"""
+    celsius = kelvin - 273.15
+    return celsius
+
+
+def convertKelvinToFahrenheit(kelvin):
+    """Converts Kelvin to Fahrenheit"""
+    fahrenheit = (kelvin - 273.15) * 9/5 + 32
     return fahrenheit

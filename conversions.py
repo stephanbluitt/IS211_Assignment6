4c852d1b54d08c2f39c4d4a5b77e86b3c3622574
Stephan Bluitt <46945764+stephanbluitt@users.noreply.github.com>
2026-03-15 17:33:33 -0400

    Refactor temperature conversion functions
    Removed unused temperature conversion functions and updated Celsius to Fahrenheit conversion.

--- conversions.py
+++ conversions.py
@@ -1,26 +1,7 @@
-# conversions
+# conversions.py
 
-#functions 
 def convertCelsiusToKelvin(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins."""
     return round(celsius + 273.15, 2)
 
 def convertCelsiusToFahrenheit(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit."""
-    return round((celsius * 9/5) + 32, 2)
-
-def convertFahrenheitToCelsius(fahrenheit):
-    """Takes in a float representing a Fahrenheit measurement, and returns that temperature converted into Celsius."""
-    return round((fahrenheit - 32) * 5/9, 2)
-
-def convertFahrenheitToKelvin(fahrenheit):
-    """Takes in a float representing a Fahrenheit measurement, and returns that temperature converted into Kelvins."""
-    return round((fahrenheit - 32) * 5/9 + 273.15, 2)
-
-def convertKelvinToCelsius(kelvin):
-    """Takes in a float representing a Kelvin measurement, and returns that temperature converted into Celsius."""
-    return round(kelvin - 273.15, 2)
-
-def convertKelvinToFahrenheit(kelvin):
-    """Takes in a float representing a Kelvin measurement, and returns that temperature converted into Fahrenheit."""
-    return round((kelvin - 273.15) * 9/5 + 32, 2)
+    return round((celsius * 9 / 5) + 32, 2)
